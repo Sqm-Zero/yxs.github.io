28a4000873f264e04df5f185f059834d2e13f4c5
沙琪马 <13005254+forgotten-seventeen@user.noreply.gitee.com>
2024-11-11 22:49:29 +0800

    初始化3

---
+++ assets/index-DqIhZ_TO.js
@@ -0,0 +1,1 @@
+import{d as n,o as a,c as r,a as e,u as c,_}from"./index-C6TyPmXy.js";const i="/yxs.github.io/assets/21-D0F8k2v2.gif",u={class:"box"},d=n({__name:"index",setup(p){let o=c();const t=()=>{o.push("/")};return(l,s)=>(a(),r("div",u,[s[0]||(s[0]=e("img",{src:i,alt:""},null,-1)),e("button",{onClick:t},"回到主页")]))}}),x=_(d,[["__scopeId","data-v-67d42824"]]);export{x as default};
