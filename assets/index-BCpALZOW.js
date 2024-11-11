dd512edcc0b1699545e09707e5d155a7f0667cac
沙琪马 <13005254+forgotten-seventeen@user.noreply.gitee.com>
2024-11-11 21:20:38 +0800

    初始化项目

---
+++ assets/index-BCpALZOW.js
@@ -0,0 +1,1 @@
+import{d as n,o as a,c as r,a as s,u as c,_}from"./index-DBpcSGDf.js";const d="/assets/21-D0F8k2v2.gif",i={class:"box"},p=n({__name:"index",setup(u){let o=c();const t=()=>{o.push("/")};return(l,e)=>(a(),r("div",i,[e[0]||(e[0]=s("img",{src:d,alt:""},null,-1)),s("button",{onClick:t},"回到主页")]))}}),f=_(p,[["__scopeId","data-v-67d42824"]]);export{f as default};
